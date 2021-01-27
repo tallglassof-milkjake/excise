4ddc8db762c86bcb93d435cd7af2a2dadb36dc54
Jake Mackinlay <jrmackinlay90@gmail.com>
2021-01-27 23:10:25 +1030

    fixed table sizing issue

--- client/src/components/SideBar/Nav.js
+++ client/src/components/SideBar/Nav.js
@@ -16,19 +16,19 @@ function NavBar() {
 
                 <div className='menu-list'>
                     <Nav.Item>
-                        <Nav.Link className='sidebar-menu-item' href="/welcome"><span><i class="fas fa-chevron-circle-down"></i></span>Home</Nav.Link>
+                        <Nav.Link className='sidebar-menu-item' href="/welcome"><span><i className="fas fa-chevron-circle-down"></i></span>Home</Nav.Link>
                     </Nav.Item>
                     <Nav.Item>
-                        <Nav.Link className='sidebar-menu-item' eventKey="link-1" href="/dashboard"><span><i class="fas fa-chevron-circle-down"></i></span>Dashboard</Nav.Link>
+                        <Nav.Link className='sidebar-menu-item' eventKey="link-1" href="/dashboard"><span><i className="fas fa-chevron-circle-down"></i></span>Dashboard</Nav.Link>
                     </Nav.Item>
                     <Nav.Item>
-                        <Nav.Link className='sidebar-menu-item' eventKey="link-2" href='/excise'><span><i class="fas fa-chevron-circle-down"></i></span>Excise</Nav.Link>
+                        <Nav.Link className='sidebar-menu-item' eventKey="link-2" href='/excise'><span><i className="fas fa-chevron-circle-down"></i></span>Excise</Nav.Link>
                     </Nav.Item>
                     <Nav.Item>
-                        <Nav.Link className='sidebar-menu-item' eventKey="link-3" href='/calculator'><span><i class="fas fa-chevron-circle-down"></i></span>Calculator</Nav.Link>
+                        <Nav.Link className='sidebar-menu-item' eventKey="link-3" href='/calculator'><span><i className="fas fa-chevron-circle-down"></i></span>Calculator</Nav.Link>
                     </Nav.Item>
                     <Nav.Item>
-                        <Nav.Link className='sidebar-menu-item' eventKey="link-4" href='/calendar'><span><i class="fas fa-chevron-circle-down"></i></span>Calendar</Nav.Link>
+                        <Nav.Link className='sidebar-menu-item' eventKey="link-4" href='/calendar'><span><i className="fas fa-chevron-circle-down"></i></span>Calendar</Nav.Link>
                     </Nav.Item>
                 </div>
             </div>
